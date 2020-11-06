c23927ed0ac8f1a796ba472df846b001458922b3
Erin Stansbury <ekstansbury@gmail.com>
2020-11-06 00:52:42 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -12,11 +12,11 @@ module FormsLab
     end
 
     post '/pirates' do
-      params["pirate"]["ships"].each do |ship_data|
-        Ship.new(ship_data["name"], ship_data["type"], ship_data["booty"])
-      end
+      @pirate = Pirate.new(params[:pirate])
 
-      @pirate = Pirate.new(params["pirate"]["name"], params["pirate"]["height"], params["pirate"]["weight"])
+      params[:pirate][:ships].each do |ship|
+        Ship.new(ship)
+      end
 
       @ships = Ship.all
 
